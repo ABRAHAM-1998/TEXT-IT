4eb4bcdf13c63662e3b2b650185ebeb216aea867
AFTERDEATH-1998 <abrahamjolly3377@gmail.com>
2021-06-16 13:53:37 +0530

    allll comoolketed to m y new koylin apps

--- app/src/main/java/com/twentytwo/textit/activities/LoginActivity.kt
+++ app/src/main/java/com/twentytwo/textit/activities/LoginActivity.kt
@@ -1,12 +1,122 @@
 package com.twentytwo.textit.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast.LENGTH_SHORT
+import android.widget.Toast.makeText
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.solver.widgets.WidgetContainer
+import androidx.constraintlayout.solver.widgets.analyzer.WidgetGroup
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.firestore.FirebaseFirestore
 import com.twentytwo.textit.R
+import com.twentytwo.textit.models.User
 
 class LoginActivity : AppCompatActivity() {
+    private var firebaseAuth: FirebaseAuth? = null
+    private var authStateListener: FirebaseAuth.AuthStateListener? = null
+    private lateinit var googleApiClient: GoogleSignInClient
+    private var rootRef: FirebaseFirestore? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        rootRef = FirebaseFirestore.getInstance()
+
+        val google_sign_in_button = findViewById<SignInButton>(R.id.google_sign_in_button)
+        google_sign_in_button.setOnClickListener { signIn() }
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        authStateListener = FirebaseAuth.AuthStateListener { auth ->
+            val firebaseUser = auth.currentUser
+            if (firebaseUser != null) {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+
+        val googleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+
+
+        googleApiClient = GoogleSignIn.getClient(this, googleSignInOptions)
+    }
+
+    private fun signIn() {
+        val signInIntent = googleApiClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val googleSignInAccount = task.getResult(ApiException::class.java)
+                if (googleSignInAccount != null) {
+                    firebaseSignInWithGoogle(googleSignInAccount)
+                }
+            } catch (e: ApiException) {
+                makeText(this, "Google sign in failed!", LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun firebaseSignInWithGoogle(googleSignInAccount: GoogleSignInAccount) {
+        val authCredential = GoogleAuthProvider.getCredential(googleSignInAccount.idToken, null)
+        firebaseAuth!!.signInWithCredential(authCredential).addOnCompleteListener(this) {
+            val firebaseUser = firebaseAuth!!.currentUser
+            if (firebaseUser != null) {
+                createUserIfNotExists(firebaseUser)
+            }
+        }
+    }
+
+    private fun createUserIfNotExists(firebaseUser: FirebaseUser) {
+        val uid = firebaseUser.uid
+        val userName = firebaseUser.displayName
+        val user = User(uid, userName!!)
+
+        val uidRef = rootRef!!.collection("users").document(uid)
+        uidRef.get().addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                val document = task.result
+                if (document != null) {
+                    if (!document.exists()) {
+                        uidRef.set(user)
+                    }
+                }
+            }
+        }
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        firebaseAuth!!.addAuthStateListener(authStateListener!!)
+    }
+
+    public override fun onStop() {
+        super.onStop()
+        if (firebaseAuth != null) {
+            firebaseAuth!!.removeAuthStateListener(authStateListener!!)
+        }
+    }
+
+    companion object {
+        //private val TAG = "LoginActivity"
+        private const val RC_SIGN_IN = 123
     }
 }
